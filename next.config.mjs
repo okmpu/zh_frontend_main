6ec37106c55346c5b6ba4cb9674262783a64a183
mukhagaliaryn <mukhagaliaryn@gmail.com>
2024-07-29 22:38:20 +0500

    Done

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,10 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['127.0.0.1', "178.128.244.196"],
+        domains: [
+            "127.0.0.1", 
+            "okmpu.oquplatforms.com"
+        ],
     },
 };
 
