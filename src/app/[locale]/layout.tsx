fbd92a1ed6bf643cbe8c9dcf7df5bf98268dc837
mukhagaliaryn <mukhagaliaryn@gmail.com>
2024-07-31 16:45:13 +0500

    Done

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -23,7 +23,7 @@ export function generateStaticParams() {
 }
 
 async function getContextData() {
-    const res = await fetch(`${process.env.BACKEND_URL}/api/main/categories/`, { cache: "force-cache"})
+    const res = await fetch(`${process.env.BACKEND_URL}/api/main/categories/`, { cache: "no-store"})
 
     if (!res.ok) {
         throw new Error('Failed to fetch data');
